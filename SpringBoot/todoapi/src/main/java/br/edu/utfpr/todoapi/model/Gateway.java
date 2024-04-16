0043b9dec725798d75edc55f4a2b88f9742a6f9b
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-04-16 17:54:43 -0300

    Gateway model
    modelo do gateway

--- SpringBoot/todoapi/src/main/java/br/edu/utfpr/todoapi/model/Gateway.java
+++ SpringBoot/todoapi/src/main/java/br/edu/utfpr/todoapi/model/Gateway.java
@@ -1,5 +1,23 @@
 package br.edu.utfpr.todoapi.model;
-//isso é um teste do git
-public class Gateway {
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_gateway") // Define o noma da tabel
+
+public class Gateway {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(name = "IP", nullable = false, unique = true)
+    private String enderecoIP;
 }
