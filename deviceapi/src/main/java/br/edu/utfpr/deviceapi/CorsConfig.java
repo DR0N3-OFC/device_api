04fb9ed9c140861b164c9c697c7544697518e96d
Bruno Raphael Facundo <dronecraftbr10@gmail.com>
2024-06-25 23:40:05 -0300

    add: Dockerfile

--- deviceapi/src/main/java/br/edu/utfpr/deviceapi/CorsConfig.java
+++ deviceapi/src/main/java/br/edu/utfpr/deviceapi/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // Add the origin of your React app
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
+            .allowedHeaders("X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization")
             .allowCredentials(true);
     }
 }
--- deviceapi/src/main/java/br/edu/utfpr/deviceapi/DeviceapiApplication.java
+++ deviceapi/src/main/java/br/edu/utfpr/deviceapi/DeviceapiApplication.java
@@ -2,8 +2,9 @@ package br.edu.utfpr.deviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class DeviceapiApplication {
 
 	public static void main(String[] args) {
